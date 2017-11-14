5390d05415f832c32cb557a16586681a96a50460
Roger Bassons <rogerbassons@gmail.com>
2017-11-13 19:49:04 -0500

    test4 tests virtual memory using malloc from threads

--- Test/test4.c
+++ Test/test4.c
@@ -1,64 +1,43 @@
 #include "../my_pthread_t.h"
 #include <stdio.h>
 
-/* this function is run by the second thread */
-void *inc_x(void *x_void_ptr)
+void *test()
 {
-
-	/* increment x to 100 */
-	int *x_ptr = (int *)x_void_ptr;
-	while(++(*x_ptr) < 100);	
-
-	printf("x increment finished\n");
-
-	int x = 500;
-	pthread_exit(&x);
-
-	/* the function must return something - NULL will do */
+	int *x = (int *)malloc(sizeof(int));
+	*x = 0;
+	while (1)
+		*x += 1;
+	pthread_exit(NULL);
 	return NULL;
-
 }
 
 int main()
 {
 
-	int *x = (int *)malloc(sizeof(int));
-	int *y = (int *)malloc(sizeof(int));
-	*x = 0;
-	*y = 0;
+ 	int nThreads = 5;
+	
+	pthread_t t[nThreads];
 
-	/* show the initial values of x and y */
-	printf("x: %d, y: %d\n", *x, *y);
-
-	/* this variable is our reference to the second thread */
-	pthread_t inc_x_thread;
-
-	/* create a second thread which executes inc_x(&x) */
-	if(pthread_create(&inc_x_thread, NULL, inc_x, x)) {
-
-		fprintf(stderr, "Error creating thread\n");
-		return 1;
-
-	}
-	/* increment y to 100 in the first thread */
-	while(++(*y) < 100);
-
-	printf("y increment finished\n");
-
-	void *p;
-
-	// wait for the second thread to finish 
-	if(pthread_join(inc_x_thread, &p)) {
-
-		fprintf(stderr, "Error joining thread\n");
-		return 2;
-
+	int i;
+	for (i = 0; i < nThreads; i++) {
+		if(pthread_create(&t[i], NULL, test, NULL)) {
+			
+			fprintf(stderr, "Error creating thread\n");
+			return 1;
+			
+		}
 	}
 
-	int z = *(int *)p;
+	for (i = 0; i < nThreads; i++) {
+		if(pthread_join(&t[i], NULL)) {
 
-	/* show the results - x is now 100 thanks to the second thread */
-	printf("x: %d, y: %d, z: %d\n", *x, *y, z);
+			fprintf(stderr, "Error joining thread\n");
+			return 2;
+
+		}
+	}
+
+
 
 	return 0;
 
