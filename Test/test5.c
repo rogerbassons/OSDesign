67d197c7fb69a043811061d8f8f8e5d2f6822e3d
Roger Bassons <rogerbassons@gmail.com>
2017-11-15 17:12:48 -0500

    test file for multiple page allocation

---
+++ Test/test5.c
@@ -0,0 +1,54 @@
+#include "../my_pthread_t.h"
+#include <stdio.h>
+#include <string.h>
+
+void *test(void *p)
+{
+
+	int size = sizeof(char) * 5000;
+	char *buff = malloc(size);
+
+	printMemory();
+	strcpy(buff, "a");
+	int i;
+	for (i = 0; i < size-2; i++) {
+		strcat(buff, "a");
+	}
+	printf("Result (%i characters): %s\n", i+1,buff);
+		
+	return NULL;
+}
+
+int main()
+{
+
+ 	int nThreads = 10;
+	
+	pthread_t t[nThreads];
+
+
+	int i;
+	for (i = 0; i < nThreads; i++) {
+		if(pthread_create(&t[i], NULL, test, NULL)) {
+			
+			fprintf(stderr, "Error creating thread\n");
+			return 1;
+			
+		}
+	}
+			
+
+	for (i = 0; i < nThreads; i++) {
+		if(pthread_join(t[i], NULL)) {
+
+			fprintf(stderr, "Error joining thread\n");
+			return 2;
+
+		}
+	}
+
+
+
+	return 0;
+
+}
